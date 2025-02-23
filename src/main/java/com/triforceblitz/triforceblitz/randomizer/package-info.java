8581010a792ff1bd4c9e7a9671e14f184db65c01
Jesse Gerard Brands <jesse@jgbrands.nl>
2025-02-23 21:43:42 +0100

    randomizer: add package-info.java

---
+++ src/main/java/com/triforceblitz/triforceblitz/randomizer/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The randomizer package offers utilities for retrieving, querying and using
+ * the Ocarina of Time Randomizer.
+ */
+@NonNullApi
+package com.triforceblitz.triforceblitz.randomizer;
+
+import org.springframework.lang.NonNullApi;
