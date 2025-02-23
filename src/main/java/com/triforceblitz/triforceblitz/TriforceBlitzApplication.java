822ca3108ecd1278da8837c8decedae8d5e54b08
Jesse Gerard Brands <jesse@jgbrands.nl>
2025-02-23 20:32:19 +0100

    project: initial project structure

---
+++ src/main/java/com/triforceblitz/triforceblitz/TriforceBlitzApplication.java
@@ -0,0 +1,11 @@
+package com.triforceblitz.triforceblitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriforceBlitzApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TriforceBlitzApplication.class, args);
+	}
+}
